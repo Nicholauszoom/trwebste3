7fdc157176bb3991201d88cbbed201523279a900
Nicholauszoom <nicholaussomi5@gmail.com>
2023-12-01 12:51:27 +0300

    my first origin

--- src/main/java/com/example/demo/email/EmailSender.java
+++ src/main/java/com/example/demo/email/EmailSender.java
@@ -2,6 +2,4 @@ package com.example.demo.email;
 
 public interface EmailSender {
     void send(String from, String email);
-    public void sendPasswordResetEmaill(String recipientEmail, String resetUrl);
-
 }
